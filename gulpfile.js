026d63fdf928ad0eb28b795def2c86547d22b844
yetihead <yetibrain@yandex.ru>
2017-09-30 17:40:21 +0300

    add dynamic html

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,25 @@ const postcss = require('gulp-postcss');
 const eslint = require('gulp-eslint');
 const stylelint = require('gulp-stylelint');
 
+const inject = require('gulp-inject');
+const runSeq = require('gulp-sequence');
+const dist = 'dist';
+
 const onFilesChange = (event) => {
   // eslint-disable-next-line no-console
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 };
 
-gulp.task('js', () => {
+gulp.task('_js', () => {
   return gulp.src('src/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dist));
 });
 
-gulp.task('css', () => {
+gulp.task('_css', () => {
   return gulp.src('src/*.css')
     .pipe(stylelint({
       reporters: [
@@ -26,12 +30,22 @@ gulp.task('css', () => {
       ]
     }))
     .pipe(postcss())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(dist));
 });
+
+gulp.task('index', () => {
+	var 
+		template = gulp.src('src/index.html'),
+		source = gulp.src([dist + '/**/*.js', dist + '/**/*.css'], {read: false});
+	return template.pipe(inject(source)).pipe(gulp.dest(dist));
+});
+
+gulp.task('css', runSeq('_css', 'index'));
+gulp.task('js', runSeq('_js', 'index'));
 
 gulp.task('watch', () => {
   gulp.watch('src/*.js', ['js']).on('change', (event) => onFilesChange(event));
   gulp.watch('src/*.css', ['css']).on('change', (event) => onFilesChange(event));
 });
 
-gulp.task('default', ['js', 'css', 'watch']);
+gulp.task('default', runSeq(['_js', '_css'], 'index', 'watch'));
