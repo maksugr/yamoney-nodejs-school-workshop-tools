8ff8cb4d7e900e2c0a2f66520b6eb1a72a5854b8
Karachev M.A <mdnkspb@mail.ru>
2017-09-30 15:09:17 +0300

    Улучшение сборки webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './styles.css';
+import printMe from './print';
+import alertInFive from './alert';
 
 // eslint-disable-next-line no-console
 console.log(`Hello, ${yamoney_nodejs_school}!`);
+alertInFive();
+
+//HMR
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    // eslint-disable-next-line no-console
+    console.log('Accepting the updated printMe module!');
+    // eslint-disable-next-line no-undef
+    printMe();
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: ['./src/index.js']
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
@@ -15,18 +19,34 @@ module.exports = {
           'eslint-loader',
           'babel-loader'
         ]
-      },
+			},
       {
         test: /\.css$/,
+				// use: ExtractTextPlugin.extract({
+				// 	fallback: 'style-loader',
+				// 	use: [
+         //    'css-loader',
+         //    'postcss-loader',
+         //  ]
+				// })
         use: [
           'style-loader',
           'css-loader',
-          'postcss-loader'
+          'postcss-loader',
         ]
-      }
+			}
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Yamoney Node.js School'
-  })]
+	plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Yamoney Node.js School'
+    }),
+		new ExtractTextPlugin("styles.css", { disable: true }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    hot: true
+  }
 };
