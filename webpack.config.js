cd6f58e52286d61c4fef898e6d7d76ce37f000c3
Alexander Bareyko <energizer@yandex-team.ru>
2017-10-02 16:57:34 +0300

    NPM scripts for lint, start, build bundling css to separate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -18,15 +19,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ]
+        use: ExtractTextPlugin.extract({
+            use: 'css-loader!postcss-loader'
+        })
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Yamoney Node.js School'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Yamoney Node.js School'
+    }),
+    new ExtractTextPlugin({
+        filename: 'styles.css'
+    })
+  ]
 };
